6b6225517e9ee6f681fb28b6ada0cd9bc20a0ab6
Shaun Smith <shaun.smith@oracle.com>
2019-11-06 16:26:48 -0500

    initial commit

---
+++ native-with-resource/src/main/java/com/example/graalvm/Greetings.java
@@ -0,0 +1,25 @@
+package com.example.graalvm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.stream.Collectors;
+
+public class Greetings {
+
+	public static void main(String[] args) throws UnsupportedEncodingException, IOException {
+		String greeting = getGreeting();
+		System.out.println(greeting);
+	}
+
+	private static String getGreeting() throws IOException, UnsupportedEncodingException {
+		try (InputStream inputStream = Greetings.class.getResourceAsStream("/greeting.txt");
+				InputStreamReader streamReader = new InputStreamReader(inputStream, "UTF-8");
+				BufferedReader bufferedReader = new BufferedReader(streamReader)) {
+			return bufferedReader.lines().collect(Collectors.joining());
+		}
+	}
+
+}
