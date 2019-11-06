f7028cf1e7f63dc33442ded38eb2f19ffa743336
Shaun Smith <shaun.smith@oracle.com>
2019-11-06 18:39:44 -0500

    tabs to spaces

--- native-resource-config/src/main/java/com/example/graalvm/Greetings.java
+++ native-resource-config/src/main/java/com/example/graalvm/Greetings.java
@@ -9,19 +9,19 @@ import java.util.stream.Collectors;
 
 public class Greetings {
 
-	private static final String GREETING_FILE = "/greeting.txt";
+    private static final String GREETING_FILE = "/greeting.txt";
 
-	public static void main(String[] args) throws UnsupportedEncodingException, IOException {
-		String greeting = getGreeting();
-		System.out.println(greeting);
-	}
+    public static void main(String[] args) throws UnsupportedEncodingException, IOException {
+        String greeting = getGreeting();
+        System.out.println(greeting);
+    }
 
-	private static String getGreeting() throws IOException, UnsupportedEncodingException {
-		try (InputStream inputStream = Greetings.class.getResourceAsStream(GREETING_FILE);
-				InputStreamReader streamReader = new InputStreamReader(inputStream, "UTF-8");
-				BufferedReader bufferedReader = new BufferedReader(streamReader)) {
-			return bufferedReader.lines().collect(Collectors.joining());
-		}
-	}
+    private static String getGreeting() throws IOException, UnsupportedEncodingException {
+        try (InputStream inputStream = Greetings.class.getResourceAsStream(GREETING_FILE);
+                InputStreamReader streamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(streamReader)) {
+            return bufferedReader.lines().collect(Collectors.joining());
+        }
+    }
 
 }
